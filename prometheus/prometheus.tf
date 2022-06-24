2212716bda1d7cfd67b776b5007a13009e38252c
Lucas González <lucasgonzalez@gmail.com>
2022-06-24 18:06:14 +0200

    [prometheus] Downsize deployment to fit into cluster

--- prometheus/prometheus.tf
+++ prometheus/prometheus.tf
@@ -2,9 +2,31 @@ terraform {
   required_version = "1.2.3"
 
   required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.11.0"
+    }
     helm = {
       source  = "hashicorp/helm"
       version = "2.6.0"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+  config_context = "kind-playground"
+}
+
+resource "kubernetes_namespace" "prometheus-namespace" {
+  lifecycle {
+    ignore_changes = [metadata]
+  }
+
+  metadata {
+    name = "prometheus"
+    labels = {
+      role   = "prometheus"
     }
   }
 }
